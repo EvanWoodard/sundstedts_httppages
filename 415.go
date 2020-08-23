2bb96016bf429efce83d39fd67b9639d892cd952
Evan Woodard <EvSunWoodard@gmail.com>
2020-08-23 13:11:57 -0500

    added 415

---
+++ 415.go
@@ -0,0 +1,33 @@
+package httppages
+
+const page415 string = `
+{{ define "415" }}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sundstedt's - 415</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link href="shared/css/theme.mobile.css" rel="stylesheet" >
+    <link href="shared/css/theme.css" rel="stylesheet" >
+</head>
+<body>
+    <div class="topbar">
+        <div class="logo"><img src="shared/img/android-chrome-512x512.png" /></div>
+        <div class="title">The Sundstedt's</div>
+        <div class="spacer"></div>
+        <form class="redirect-form" action="https://iam.sundstedt.us/logout" method="POST">
+            <input id="home_redirect" type="hidden" name="redirect" value="https://evenson.sundstedt.us/">
+            <button class="btn login-btn" type="submit">Home</button>
+        </form>
+    </div>
+    <div class="page-content">
+        <div class="http-error">
+            <strong class="code">415</strong>
+            <div class="message">You cannot navigate directly to this page. Sorry!</div>
+        </div>
+    </div>
+</body>
+</html>
+{{ end }}
+`
