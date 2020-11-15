5d208bc6868f5d0b6628c17a2d2d9b7005a55547
Evan Woodard <EvSunWoodard@gmail.com>
2020-11-14 22:40:21 -0600

    updated urls

--- 415.go
+++ 415.go
@@ -8,13 +8,13 @@ const page415 string = `
     <title>Sundstedt's - 415</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    <link href="https://res.sundstedt.us/css/theme.light.css" rel="stylesheet">
-    <link href="https://res.sundstedt.us/css/mobile.css" rel="stylesheet">
-    <link href="https://res.sundstedt.us/css/standard.css" rel="stylesheet">
+    <link href="https://cdn.sundstedt.us/css/theme.light.css" rel="stylesheet">
+    <link href="https://cdn.sundstedt.us/css/mobile.css" rel="stylesheet">
+    <link href="https://cdn.sundstedt.us/css/standard.css" rel="stylesheet">
 </head>
 <body>
     <div class="topbar">
-        <div class="logo"><img src="https://res.sundstedt.us/img/android-chrome-512x512.png" /></div>
+        <div class="logo"><img src="https://cdn.sundstedt.us/img/android-chrome-512x512.png" /></div>
         <div class="title">The Sundstedt's</div>
         <div class="spacer"></div>
         <form class="redirect-form" action="https://iam.sundstedt.us/logout" method="POST">
